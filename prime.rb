8afb41011044f4f2b0d2de8a73b95521347c5d9e
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-03 20:42:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
-# Add  code here!
-def prime?(integer)
-  range = (2..integer-1)
-  array = range.to_a
-    if num < 2
-       return false
-    else
+def prime?(num)
+  range = (2..num-1)
+  array = range.to_a    
+    if num < 2 
+       return false 
+    else 
       array.all? do |x|
         num % x != 0  
-      end
     end
+  end
 end
